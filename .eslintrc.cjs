df5aad80b148f39712dee58ae9f26b8a3b5d5d2a
tingruchen104 <tingru.chen@104.com.tw>
2026-07-20 14:19:56 +0800

    reset

--- .eslintrc.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  env: { browser: true, es2022: true },
-  extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', 'prettier'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  rules: {
-    'vue/multi-word-component-names': 'off'
-  }
-}
